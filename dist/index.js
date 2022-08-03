65e6c23d195f973bd236f7eab05c93828adeff2f
me-safty <atef50895@gmail.com>
2022-08-03 08:27:43 +0200

    make game functionality

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,100 @@
 "use strict";
 // make images clickable with adding animation
 const boxs = document.querySelectorAll(".box");
+const imgs = document.querySelectorAll("img");
+const high = document.querySelectorAll(".header p span");
+if (localStorage.high_score) {
+    high[1].innerHTML = `${localStorage.high_score}`;
+}
+const fin = document.querySelector(".finsh");
+const finBox = document.querySelector(".finBox");
+const finBoxHigh = document.querySelectorAll(".finBox p")[1]
+    .children[0];
+const finBoxYour = document.querySelectorAll(".finBox p")[2]
+    .children[0];
+const btn = document.querySelector(".finBox button");
+btn.addEventListener("click", () => location.reload());
+setTimeout(() => {
+    imgs.forEach((e) => (e.style.opacity = "1"));
+    fin.style.display = "block";
+}, 500);
+setTimeout(() => {
+    imgs.forEach((e) => (e.style.opacity = "0"));
+    fin.style.display = "none";
+}, 2500);
+let wrongTreis = 0;
 boxs.forEach((box) => {
     box.addEventListener("click", (_) => {
-        box.style.animation = "rotate .3s both";
-        const img = box.childNodes[1];
-        img.style.animation = "op .3s both";
+        var _a, _b;
+        if (box.classList.contains("done") === false) {
+            box.style.animation = "rotate .3s both";
+            const img = box.childNodes[1];
+            img.style.display = "block";
+            img.style.opacity = "1";
+            img.classList.add("active");
+            //=======////
+            const imgActive = document.querySelectorAll(".active");
+            if (imgActive.length === 2) {
+                const img1 = (_a = imgActive[0]
+                    .getAttribute("src")) === null || _a === void 0 ? void 0 : _a.split("/")[1].split(".")[0];
+                const img2 = (_b = imgActive[1]
+                    .getAttribute("src")) === null || _b === void 0 ? void 0 : _b.split("/")[1].split(".")[0];
+                setTimeout(() => {
+                    var _a, _b;
+                    if (img1 != img2) {
+                        imgActive[0].style.display = "none";
+                        imgActive[1].style.display = "none";
+                        const one = imgActive[0].parentElement;
+                        const two = imgActive[1].parentElement;
+                        one.style.animation = "wrongRotate .3s both";
+                        two.style.animation = "wrongRotate .3s both";
+                        wrongTreis++;
+                        const wrongTreisP = document.querySelectorAll(".header p span");
+                        wrongTreisP[2].innerHTML = `${wrongTreis}`;
+                    }
+                    else {
+                        (_a = imgActive[0].parentElement) === null || _a === void 0 ? void 0 : _a.classList.add("done");
+                        (_b = imgActive[1].parentElement) === null || _b === void 0 ? void 0 : _b.classList.add("done");
+                        finshGame();
+                    }
+                    imgActive[0].classList.remove("active");
+                    imgActive[1].classList.remove("active");
+                }, 300);
+            }
+        }
     });
 });
-// randomise images
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const imags = document.querySelectorAll("img");
-for (let i = 0; i < 20; i++) {
-    const random = Math.floor(Math.random() * arr.length);
-    imags[i].src = `images/${arr[random]}.jpg`;
-    let index = arr.indexOf(arr[random]);
-    arr.splice(index, 1);
+function finshGame() {
+    const boxsDone = document.querySelectorAll(".done");
+    if (boxsDone.length == 20) {
+        if (!localStorage.high_score) {
+            window.localStorage.setItem("high_score", `${wrongTreis}`);
+        }
+        if (localStorage.high_score) {
+            if (+localStorage.high_score > wrongTreis) {
+                high[1].innerHTML = `${wrongTreis}`;
+            }
+            else {
+                high[1].innerHTML = `${localStorage.high_score}`;
+            }
+        }
+        else {
+            high[1].innerHTML = `${wrongTreis}`;
+        }
+        fin.style.display = "flex";
+        fin.style.opacity = "1";
+        finBoxHigh.innerHTML = high[1].innerHTML;
+        finBoxYour.innerHTML = `${wrongTreis}`;
+    }
 }
+// randomise images
+window.onload = (_) => {
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const imags = document.querySelectorAll("img");
+    for (let i = 0; i < 20; i++) {
+        const random = Math.floor(Math.random() * arr.length);
+        imags[i].src = `images/${arr[random]}.jpg`;
+        let index = arr.indexOf(arr[random]);
+        arr.splice(index, 1);
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,118 @@
 // make images clickable with adding animation
 const boxs = document.querySelectorAll(".box") as NodeListOf<HTMLDivElement>;
+const imgs = document.querySelectorAll("img") as NodeListOf<HTMLImageElement>;
+
+const high = document.querySelectorAll(
+  ".header p span"
+) as NodeListOf<HTMLSpanElement>;
+
+if (localStorage.high_score) {
+  high[1].innerHTML = `${localStorage.high_score}`;
+}
+
+const fin = document.querySelector(".finsh") as HTMLDivElement;
+const finBox = document.querySelector(".finBox") as HTMLDivElement;
+const finBoxHigh = document.querySelectorAll(".finBox p")[1]
+  .children[0] as HTMLSpanElement;
+const finBoxYour = document.querySelectorAll(".finBox p")[2]
+  .children[0] as HTMLSpanElement;
+const btn = document.querySelector(".finBox button") as HTMLButtonElement;
+btn.addEventListener("click", () => location.reload());
+
+setTimeout(() => {
+  imgs.forEach((e) => (e.style.opacity = "1"));
+  fin.style.display = "block";
+}, 500);
+setTimeout(() => {
+  imgs.forEach((e) => (e.style.opacity = "0"));
+  fin.style.display = "none";
+}, 2500);
+
+let wrongTreis: number = 0;
 
 boxs.forEach((box) => {
   box.addEventListener("click", (_) => {
-    box.style.animation = "rotate .3s both";
-    const img = box.childNodes[1] as HTMLImageElement;
-    img.style.animation = "op .3s both";
+    if (box.classList.contains("done") === false) {
+      box.style.animation = "rotate .3s both";
+      const img = box.childNodes[1] as HTMLImageElement;
+      img.style.display = "block";
+      img.style.opacity = "1";
+      img.classList.add("active");
+      //=======////
+      const imgActive = document.querySelectorAll(
+        ".active"
+      ) as NodeListOf<HTMLImageElement>;
+      if (imgActive.length === 2) {
+        const img1 = imgActive[0]
+          .getAttribute("src")
+          ?.split("/")[1]
+          .split(".")[0] as string;
+        const img2 = imgActive[1]
+          .getAttribute("src")
+          ?.split("/")[1]
+          .split(".")[0] as string;
+        setTimeout(() => {
+          if (img1 != img2) {
+            imgActive[0].style.display = "none";
+            imgActive[1].style.display = "none";
+            const one = imgActive[0].parentElement as HTMLDivElement;
+            const two = imgActive[1].parentElement as HTMLDivElement;
+            one.style.animation = "wrongRotate .3s both";
+            two.style.animation = "wrongRotate .3s both";
+            wrongTreis++;
+            const wrongTreisP = document.querySelectorAll(
+              ".header p span"
+            ) as NodeListOf<HTMLSpanElement>;
+            wrongTreisP[2].innerHTML = `${wrongTreis}`;
+          } else {
+            imgActive[0].parentElement?.classList.add("done");
+            imgActive[1].parentElement?.classList.add("done");
+            finshGame();
+          }
+          imgActive[0].classList.remove("active");
+          imgActive[1].classList.remove("active");
+        }, 300);
+      }
+    }
   });
 });
 
-// randomise images
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-const imags = document.querySelectorAll("img") as NodeListOf<HTMLImageElement>;
-
-for (let i = 0; i < 20; i++) {
-  const random: number = Math.floor(Math.random() * arr.length);
-  imags[i].src = `images/${arr[random]}.jpg`;
-  let index: number = arr.indexOf(arr[random]);
-  arr.splice(index, 1);
+function finshGame() {
+  const boxsDone = document.querySelectorAll(
+    ".done"
+  ) as NodeListOf<HTMLDivElement>;
+  if (boxsDone.length == 20) {
+    if (!localStorage.high_score) {
+      window.localStorage.setItem("high_score", `${wrongTreis}`);
+    }
+    if (localStorage.high_score) {
+      if (+localStorage.high_score > wrongTreis) {
+        high[1].innerHTML = `${wrongTreis}`;
+      } else {
+        high[1].innerHTML = `${localStorage.high_score}`;
+      }
+    } else {
+      high[1].innerHTML = `${wrongTreis}`;
+    }
+    fin.style.display = "flex";
+    fin.style.opacity = "1";
+    finBoxHigh.innerHTML = high[1].innerHTML;
+    finBoxYour.innerHTML = `${wrongTreis}`;
+  }
 }
+
+// randomise images
+window.onload = (_) => {
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  const imags = document.querySelectorAll(
+    "img"
+  ) as NodeListOf<HTMLImageElement>;
+
+  for (let i = 0; i < 20; i++) {
+    const random: number = Math.floor(Math.random() * arr.length);
+    imags[i].src = `images/${arr[random]}.jpg`;
+    let index: number = arr.indexOf(arr[random]);
+    arr.splice(index, 1);
+  }
+};
